fa505394495e350b8f11e134b5a41b41b00b9b8f
Dr.Slink <slink21@mail.ru>
2019-05-21 18:54:22 +0300

    Initial commit

---
+++ app/src/main/java/com/dr/slink/hands/Camera2Fragment.java
@@ -0,0 +1,6 @@
+package com.dr.slink.hands;
+
+import android.support.v4.app.Fragment;
+
+public class Camera2Fragment extends Fragment {
+}
---
+++ app/src/main/java/com/dr/slink/hands/HandsMainActivity.java
@@ -0,0 +1,19 @@
+package com.dr.slink.hands;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HandsMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, new Camera2Fragment())
+                    .commit();
+        }
+    }
+}
